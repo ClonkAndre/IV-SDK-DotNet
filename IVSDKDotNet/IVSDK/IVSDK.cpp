906c0e7f45e45eda14134a7201fc1fbdb9a249c5
E. S <39125931+ClonkAndre@users.noreply.github.com>
2022-10-22 17:39:07 +0200

    Add files via upload

---
+++ IVSDKDotNet/IVSDK/IVSDK.cpp
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define UNICODE
+#pragma comment(lib, "version.lib")
+#include <windows.h>
+#include <stdint.h>
+#include <string>
+#include <list>
+#include <d3dx9.h>
+#include "injector/injector.hpp"
+
+#include "Addresses.h"
+#include "IVSDK.h"
+#include "Hooks.h"
+
+namespace plugin
+{
+	HMODULE GetCurrentModule()
+	{
+		HMODULE hModule = NULL;
+		GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, (LPCTSTR)GetCurrentModule, &hModule);
+		return hModule;
+	}
+
+	uint32_t DoHook(uint32_t address, void(*Function)())
+	{
+		if (address)
+		{
+			uint32_t origcall = (uint32_t)injector::ReadRelativeOffset(address + 1);
+
+			injector::MakeCALL(address, Function);
+
+			return origcall;
+		}
+		return 0;
+	}
+	void InitHooks()
+	{
+		processScriptsEvent::returnAddress = DoHook(AddressSetter::Get(0x21601, 0x95141), processScriptsEvent::MainHook);
+		gameLoadEvent::returnAddress = DoHook(AddressSetter::Get(0x4ADB38, 0x770748), gameLoadEvent::MainHook);
+		gameLoadPriorityEvent::returnAddress = DoHook(AddressSetter::Get(0x4ADA9D, 0x7706AD), gameLoadPriorityEvent::MainHook);
+		drawingEvent::returnAddress = DoHook(AddressSetter::Get(0x46AFA8, 0x60E1C8), drawingEvent::MainHook);
+		processAutomobileEvent::callAddress = DoHook(AddressSetter::Get(0x7FE9C6, 0x652C26), processAutomobileEvent::MainHook);
+		processPadEvent::callAddress = DoHook(AddressSetter::Get(0x3C4002, 0x46A802), processPadEvent::MainHook);
+		processCameraEvent::returnAddress = DoHook(AddressSetter::Get(0x52C4C2, 0x694232), processCameraEvent::MainHook);
+		mountDeviceEvent::returnAddress = DoHook(AddressSetter::Get(0x3B2E27, 0x456C27), mountDeviceEvent::MainHook);
+	}
+	void Init()
+	{
+		if (!AddressSetter::bAddressesRead) AddressSetter::Init();
+		if (gameVer != VERSION_NONE)
+		{
+			InitHooks();
+
+			gameStartupEvent();
+		}
+	}
+}
+
+BOOL APIENTRY DllMain(HMODULE, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call) {
+		case DLL_PROCESS_ATTACH:
+			plugin::Init();
+			break;
+	}
+	return TRUE;
+}
+
---
+++ IVSDKDotNet/dllmain.cpp
@@ -0,0 +1,71 @@
+#include "IVSDK.cpp"
+
+#pragma region Variables
+HANDLE managedThreadHandle;
+#pragma endregion
+
+#pragma region Events
+void ScriptLoop()
+{
+	CLR::CLRBridge::InvokeTickEvents();
+}
+void GameLoad()
+{
+	CLR::CLRBridge::InvokeGameLoadEvents();
+}
+void GameLoadPriority()
+{
+	CLR::CLRBridge::InvokeGameLoadPriorityEvents();
+}
+void MountDevice()
+{
+	CLR::CLRBridge::InvokeMountDeviceEvents();
+}
+void Drawing()
+{
+	CLR::CLRBridge::InvokeDrawingEvents();
+}
+void ProcessCamera()
+{
+	CLR::CLRBridge::InvokeProcessCameraEvents();
+}
+void ProcessAutomobile(CVehicle* veh)
+{
+	// Todo: Make class CVehicle managed and add to parameter of method below.
+	CLR::CLRBridge::InvokeProcessAutomobileEvents();
+}
+void ProcessPad(CPad* pad)
+{
+	// Todo: Make class CPad managed and add to parameter of method below.
+	CLR::CLRBridge::InvokeProcessPadEvents();
+}
+#pragma endregion
+
+// Managed Entry Point Thread
+DWORD WINAPI ManagedEntryPoint(HMODULE hModule)
+{
+	CLR::CLRBridge::Initialize((int)plugin::gameVer, AddressSetter::gBaseAddress);
+
+	while (true) {
+		Sleep(1000);
+	}
+	FreeLibraryAndExitThread(hModule, 0);
+	return 0;
+}
+
+// IV-SDK Stuff
+void plugin::gameStartupEvent()
+{
+	// Launch a thread to get a managed entry point and to keep the plugin alive
+	managedThreadHandle = CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)&ManagedEntryPoint, GetCurrentModule(), 0, nullptr);
+
+	// Subscribe to IV-SDK Events
+	plugin::processScriptsEvent::Add(ScriptLoop);
+	plugin::gameLoadEvent::Add(GameLoad);
+	plugin::gameLoadPriorityEvent::Add(GameLoadPriority);
+	plugin::mountDeviceEvent::Add(MountDevice);
+	plugin::drawingEvent::Add(Drawing);
+	plugin::processCameraEvent::Add(ProcessCamera);
+	plugin::processAutomobileEvent::Add(ProcessAutomobile);
+	plugin::processPadEvent::Add(ProcessPad);
+}
