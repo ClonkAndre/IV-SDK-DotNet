7e2126b2cbf2957a3cc9af505beea36d2fc71425
E. S <39125931+ClonkAndre@users.noreply.github.com>
2022-10-24 16:53:37 +0200

    Updated example to v0.2

--- TestScript/Main.cs
+++ TestScript/Main.cs
@@ -9,21 +9,33 @@ using static IVSDKDotNet.Native.Natives;
 namespace TestScript {
     public class Main : Script {
 
+        #region Variables
+        private int playerPed;
+        #endregion
+
         #region Constructor
         public Main()
         {
-            // Register a custom console command for this script
-            CGame.Console.RegisterCommand("Test", (string[] args) => { CGame.Console.PrintWarning("Custom command 'Test' called!"); });
-
+            Initialized += Main_Initialized;
             Tick += Main_Tick;
             KeyDown += Main_KeyDown;
         }
         #endregion
 
+        private void Main_Initialized(object sender, EventArgs e)
+        {
+            // Register a custom console command for this script
+            CGame.Console.RegisterCommand(this, "Test", (string[] args) => { CGame.Console.PrintWarning("Custom command 'Test' called!"); });
+        }
+
         // Runs every frame when in-game
         private void Main_Tick(object sender, EventArgs e)
         {
-            // - - - - Some tests - - - -
+            // Get player index and get player
+            int playerID = CONVERT_INT_TO_PLAYERINDEX(GET_PLAYER_ID());
+            GET_PLAYER_CHAR(playerID, out playerPed);
+
+            // - - - - - - - Some tests - - - - - - -
 
             //int index = (int)GET_PLAYER_RADIO_STATION_INDEX();
             //CRadioStation rs = CRadioStation.GetRadioStation(index);
@@ -36,6 +48,8 @@ namespace TestScript {
             //if (r != null) {
             //    ShowSubtitleMessage(r.CurrentRadioStation.ToString(), 1000);
             //}
+
+            //ShowSubtitleMessage(string.Format("Minute: {0}", MemoryAccess.GetRef<uint>(0xDD52FC, 0xD51690).ToString()));
         }
 
         private void Main_KeyDown(object sender, KeyEventArgs e)
@@ -43,34 +57,39 @@ namespace TestScript {
             // Creates an explosion infront of the player
             if (e.KeyCode == Keys.NumPad9) {
 
-                // Get player index and get player
-                int playerID = CONVERT_INT_TO_PLAYERINDEX(GET_PLAYER_ID());
-                GET_PLAYER_CHAR(playerID, out int playerPed);
-
                 // Get player coordinates
-                GET_CHAR_COORDINATES(playerPed, out float x, out float y, out float z);
+                GET_CHAR_COORDINATES(playerPed, out Vector3 pos);
 
                 // Get player heading
                 GET_CHAR_HEADING(playerPed, out float heading);
 
                 // Get position infront of player
-                Vector3 v = GetPositionInFrontOfPed(new Vector3(x, y, z), HeadingToDirection(heading));
+                Vector3 v = Helper.GetPositionInFrontOfEntity(pos, Helper.HeadingToDirection(heading), 12f);
 
                 // Create explosion
                 ADD_EXPLOSION(v.X, v.Y, v.Z, (int)eExplosionType.EXPLOSION_TYPE_ROCKET, 10f, true, false, 0.5f);
 
             }
-        }
+            if (e.KeyCode == Keys.NumPad7) {
 
-        // These will later be implemented in IV-SDK .NET so you don't have to add them manually
-        public Vector3 GetPositionInFrontOfPed(Vector3 pedPos, Vector3 pedDir, float onFootMultiplier = 12f)
-        {
-            return Vector3.Add(pedPos, Vector3.Multiply(pedDir, onFootMultiplier));
-        }
-        public Vector3 HeadingToDirection(float heading)
-        {
-            float h = heading * (float)(Math.PI / 180);
-            return new Vector3((float)-Math.Sin(h), (float)Math.Cos(h), 0f);
+                // Explodes every ped except the player
+                int[] peds = CPools.GetAllPedHandles();
+                for (int i = 0; i < peds.Length; i++) {
+                    int ped = peds[i];
+                    if (ped != 0 && ped != playerPed) {
+                        if (!IS_CHAR_DEAD(ped)) {
+
+                            // Get ped coordinates
+                            GET_CHAR_COORDINATES(ped, out Vector3 pos);
+
+                            // Create explosion
+                            ADD_EXPLOSION(pos.X, pos.Y, pos.Z, (int)eExplosionType.EXPLOSION_TYPE_ROCKET, 10f, true, false, 0.5f);
+
+                        }
+                    }
+                }
+
+            }
         }
 
     }
