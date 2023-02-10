68a33bc4f2262d273321aae171abfef2f5a11862
E. S <39125931+ClonkAndre@users.noreply.github.com>
2023-02-11 00:20:33 +0100

    v0.5 update

--- TestScript/Main.cs
+++ TestScript/Main.cs
@@ -2,17 +2,41 @@
 using System.Drawing;
 using System.Globalization;
 using System.Numerics;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 using IVSDKDotNet;
+using IVSDKDotNet.Direct3D9;
 using IVSDKDotNet.Enums;
 using static IVSDKDotNet.Native.Natives;
 
 namespace TestScript {
     public class Main : Script {
 
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT {
+            public int X;
+            public int Y;
+
+            public override string ToString()
+            {
+                return string.Format("[X: {0}, Y: {1}]", X.ToString(), Y.ToString());
+            }
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(out POINT point);
+
         #region Variables
         private int playerPed;
+
+        private Vector2 mousePos;
+        private string text = "-";
+
+        private D3DGraphics gfx;
+        private D3DResource testTexture;
+        private D3DResource testFont;
         #endregion
 
         #region Constructor
@@ -27,9 +51,10 @@ namespace TestScript {
 
         private void Main_Initialized(object sender, EventArgs e)
         {
-            // Register a custom console command for this script
-            CGame.Console.RegisterCommand(this, "Test", (string[] args) => { CGame.Console.PrintWarning("Custom command 'Test' called!"); });
-            
+            // 2 ways to register a custom console command for this script
+            RegisterConsoleCommand("Test1",                 (string[] args) => { CGame.Console.PrintWarning("Custom command 'Test1' called!"); });
+            CGame.Console.RegisterCommand(this, "Test2",    (string[] args) => { CGame.Console.PrintWarning("Custom command 'Test2' called!"); });
+
             // Read TestScripts settings file (TestScript.ini)
             if (Settings != null) {
                 CGame.Console.PrintWarning("bool: "    + Settings.GetBoolean("Section1", "bool", false).ToString());
@@ -50,11 +75,74 @@ namespace TestScript {
 
                 }
             }
+
+            // Init graphics object
+            gfx = new D3DGraphics(this);
+            gfx.OnInit += Gfx_OnInit;
+            gfx.OnDeviceEndScene += Gfx_OnDeviceEndScene;
+        }
+
+        // DirectX stuff
+        private void Gfx_OnInit(IntPtr device)
+        {
+            // Try to create texture
+            D3DResult r = gfx.CreateD3D9Texture(device, Properties.Resources.discordTestLogo);
+            if (r.Error != null) {
+                CGame.Console.PrintError("(TestScript) Error while creating texture: " + r.Error.ToString());
+            }
+            else {
+                testTexture = (D3DResource)r.DXObject;
+                CGame.Console.Print(string.Format("(TestScript) Created texture with ID: {0} Handle: {1} Properties: {2}", r.ID.ToString(), testTexture.Handle.ToString(), testTexture.ResourceProperties));
+            }
+
+            // Try to create font
+            r = gfx.CreateD3D9Font(device, D3DFontDescription.Default());
+            if (r.Error != null) {
+                CGame.Console.PrintError("(TestScript) Error while creating font: " + r.Error.ToString());
+            }
+            else {
+                testFont = (D3DResource)r.DXObject;
+                CGame.Console.Print(string.Format("(TestScript) Created font with ID: {0} Handle: {1}", r.ID.ToString(), testFont.Handle.ToString()));
+            }
+        }
+        private void Gfx_OnDeviceEndScene(IntPtr device)
+        {
+            gfx.DrawLine(device, new Vector2(10f, 10f), new Vector2(50f, 100f), Color.Red, 5f);
+            gfx.DrawTexture(device, testTexture, mousePos, Size.Empty, new Vector2(0.2f, 0.2f), Color.White);
+            gfx.DrawString(device, testFont, text, 50, 50, Color.Red);
+
+            gfx.DrawBox(device, new Vector2(128f, 128f), new SizeF(64f, 64f), 4f, Color.Red);
+            gfx.DrawBoxBordered(device, new Vector2(300f, 128f), new SizeF(64f, 64f), 4f, Color.FromArgb(100, Color.Blue), Color.Red);
+            gfx.DrawBoxFilled(device, new Vector2(400f, 128f), new SizeF(64f, 64f), Color.Red);
+            gfx.DrawBoxRounded(device, new Vector2(500f, 200f), new SizeF(64f, 64f), 5f, false, Color.FromArgb(100, Color.Blue), Color.Red);
+            gfx.DrawBoxRounded(device, new Vector2(600f, 200f), new SizeF(64f, 64f), 5f, true, Color.FromArgb(100, Color.Blue), Color.Red);
+            gfx.DrawCircle(device, new Vector2(700f, 200f), 16f, 0f, eD3DCircleType.Full, true, 32, Color.Red);
+            gfx.DrawCircle(device, new Vector2(500f, 400f), 16f, 0f, eD3DCircleType.Half, true, 32, Color.Red);
+            gfx.DrawCircle(device, new Vector2(200f, 400f), 16f, 0f, eD3DCircleType.Quarter, true, 32, Color.Red);
+            gfx.DrawCircleFilled(device, new Vector2(600f, 500f), 16f, 0f, eD3DCircleType.Quarter, true, 32, Color.Red);
+
+            Vector2[] vertices = new Vector2[4] {
+                new Vector2(0f, 0f),
+                new Vector2(200f, 300f),
+                new Vector2(700f, 700f),
+                new Vector2(500f, 1000f)
+            };
+            gfx.DrawLines(device, vertices, Color.Green, false, 5f);
         }
 
         // Runs every frame when in-game
         private void Main_Tick(object sender, EventArgs e)
         {
+            bool leftMouseButton = IsKeyPressed(Keys.LButton);
+            bool rightMouseButton = IsKeyPressed(Keys.RButton);
+
+            if (GetCursorPos(out POINT p)) {
+                mousePos = new Vector2(p.X, p.Y);
+            }
+
+            GET_MOUSE_WHEEL(out int wheel);
+            text = string.Format("leftMouseButton: {0}, rightMouseButton: {1}, Wheel: {2}, Pos: {3}", leftMouseButton.ToString(), rightMouseButton.ToString(), wheel.ToString(), p.ToString());
+
             // Get player index, get player char and get char coordinates
             int playerID = CONVERT_INT_TO_PLAYERINDEX(GET_PLAYER_ID());
             GET_PLAYER_CHAR(playerID, out playerPed);
@@ -85,9 +173,20 @@ namespace TestScript {
 
             // Gets the nearest street node position from the players position
             if (IsKeyPressed(Keys.U)) {
-                Vector3 res = CWorld.GetNextPositionOnStreet(playerPos);
-                DRAW_CHECKPOINT(res, 4f, Color.Green);
+                Vector3 v = CWorld.GetNextPositionOnStreet(playerPos);
+                DRAW_CHECKPOINT(v, 4f, Color.Green);
             }
+
+            //if (IsKeyPressed(Keys.K)) {
+            //    CVehicle[] vehicles = CPools.GetAllVehicles();
+            //    for (int i = 0; i < vehicles.Length; i++) {
+            //        CVehicle vehicle = vehicles[i];
+            //        if (vehicle != null) {
+            //            vehicle.PrimaryColor = (byte)rnd.Next(0, 255);
+            //            vehicle.SecondaryColor = (byte)rnd.Next(0, 255);
+            //        }
+            //    }
+            //}
 
             // Teleports all objects currently in the world to the players position. The physics matrix of the objects will stay untouched.
             //if (IsKeyPressed(Keys.O)) {
@@ -99,12 +198,10 @@ namespace TestScript {
             //        }
             //    }
             //}
-
         }
 
         private void Main_KeyDown(object sender, KeyEventArgs e)
         {
-
             // Creates an explosion infront of the player
             if (e.KeyCode == Keys.NumPad9) {
 
@@ -185,7 +282,6 @@ namespace TestScript {
                 }
 
             }
-
         }
 
     }
